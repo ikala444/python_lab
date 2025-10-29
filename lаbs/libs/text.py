d9814349004cf204cf1c603cd8de367973815ecd
ikala444 <164887716+ikala444@users.noreply.github.com>
2025-10-29 22:52:02 +0300

    update

---
+++ lаbs/libs/text.py
@@ -0,0 +1,47 @@
+import re
+
+def normalize(text: str, *, casefold: bool = True, yo2e: bool = True) -> str:
+    text = re.sub(r"[\x00-\x1F\x7F]+", " ", text) #перебирает ASCII символы с кодами от 0 до 31, 127
+    text = text.casefold() if casefold else text 
+    text = re.sub(r"\s+", " ", text.strip())
+    text = text.replace("ё", "е") if yo2e else text
+    return text
+
+def tokenize(text: str) -> list[str]:
+    allowed_chars = 'a-zA-Zа-яёА-ЯЯЁ0-9- '
+    text = re.sub(f'[^{allowed_chars}]', ' ', text)
+    text = text.strip()
+    text = re.sub(r'\s+', ' ', text)
+    return text.split()
+
+def count_freq(tokens: list[str]) -> dict[str, int]:
+    counts = {}
+    for tok in tokens:
+        counts[tok] = counts.get(tok, 0) + 1 #функция задаёт ключу словаря значение, сначала считает их количество в словаре,а потом добавляет к нему 1 и сохраняет в словаре
+    return counts
+
+def top_n(freq: dict[str, int], n: int = 2) -> list[tuple[str, int]]:
+    words = list(sorted(freq.items(), key = lambda x: (-x[1], x[0])))[:n]
+    return words
+
+
+# normalize
+assert normalize("ПрИвЕт\nМИр\t") == "привет мир"
+assert normalize("ёжик, Ёлка") == "ежик, елка"
+
+# tokenize
+assert tokenize("привет, мир!") == ["привет", "мир"]
+assert tokenize("по-настоящему круто") == ["по-настоящему", "круто"]
+assert tokenize("2025 год") == ["2025", "год"]
+
+# count_freq + top_n
+freq = count_freq(["a","b","a","c","b","a"])
+assert freq == {"a":3, "b":2, "c":1}
+assert top_n(freq, 2) == [("a",3), ("b",2)]
+
+# тай-брейк по слову при равной частоте
+freq2 = count_freq(["bb","aa","bb","aa","cc"])
+assert top_n(freq2, 2) == [("aa",2), ("bb",2)]
+
+import sys
+print(sys.path)
